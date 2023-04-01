3ea088a8d363d17225101f853f75c26fd7db82c1
MNS81 <mns81@bk.ru>
2023-04-02 01:35:07 +0300

    Калькулятор

--- RemoveStringsWithKeys.py
+++
@@ -1,6 +0,0 @@
-with open('data/in.txt', encoding='utf-8') as f_in, open('data/minus.txt', encoding='utf-8') as f_minus, open('data/out.txt', 'w', encoding='utf-8') as f_out:
-    lst_in = f_in.read().splitlines()
-    lst_minus = f_minus.read().splitlines()
-    for row in lst_in:
-        if any(word in row for word in lst_minus): lst_in.remove(row)
-    print(*lst_in, file=f_out, sep='\n')
--- VolAndSquraPyramid.py
+++
@@ -1,9 +0,0 @@
-from math import sqrt
-# Объём пирамиды
-def volume(a, h): return round(a ** 2 * h / (4 * sqrt(3)), 3)
-# Площадь пирамиды
-def square(a, h): return round(a ** 2 * sqrt(3) / 4 + 3 * a / 2 * sqrt(h ** 2 + a ** 2 / 12), 3)
-
-a, h = float(input('Длина основания: ')), float(input('Высота: '))
-if a <= 0 or h <= 0: print('error')
-else: print(volume(a, h), square(a, h))
