87a2bee1a21c1219d81fa97b9bdc56fc1de621a4
MNS81 <mns81@bk.ru>
2023-04-01 23:02:00 +0300

    Калькулятор

---
+++ Calculator.py
@@ -0,0 +1,26 @@
+print('*** Calculator ***')
+print()
+print('''Доступные операции:
+\t+ - сложение
+\t- - вычитание
+\t* - умножение
+\t/ - деление
+\t% - остаток от деления
+\t// - целочисленное деление
+\t^ - возведение в степень''')
+print('Пример ввода: a * b, пробелы обязательны!')
+print()
+a, oper, b = input('>>> ').split()
+print()
+a = int(a)
+b = int(b)
+match oper:
+    case '+': print(f'{a} + {b} = {a + b}')
+    case '-': print(f'{a} - {b} = {a - b}')
+    case '*': print(f'{a} * {b} = {a * b}')
+    case '/': print(f'{a} / {b} = {a / b}')
+    case '%': print(f'{a} % {b} = {a % b}')
+    case '//': print(f'{a} // {b} = {a // b}')
+    case '^': print(f'{a} ^ {b} = {a ** b}')
+    case default: print('Неизвестная операция')
+input()
