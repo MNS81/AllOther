1dd4c34fb09335d03529379469e26bd869be54c4
MNS81 <mns81@bk.ru>
2023-04-03 01:29:39 +0300

    Программа удаляет из файла "in.txt" все строки, в которых содержатся ключи из файла "minus.txt".

--- files/JoinKeys.py
+++ files/JoinKeys.py
@@ -1,3 +1,5 @@
+import time
+from progress.bar import IncrementalBar
 with open('data/1.txt', encoding='utf-8') as first, open('data/2.txt', encoding='utf-8') as second, open('data/out.txt', 'w', encoding='utf-8') as outfile:
     print('*** Join Keys ***')
     print()
@@ -10,6 +12,10 @@ with open('data/1.txt', encoding='utf-8') as first, open('data/2.txt', encoding=
     for word in lst_1:
         for key in lst_2:
             lst_out.append(f'{word} {key}')
+    i_bar = IncrementalBar('Пум-пум-пи-дум...', max=len(lst_out))
     for row in sorted(lst_out):
         outfile.write(row + '\n')
-input(f'Список уникальных комбинаций сохранен в файл "out.txt" в папке "data".\n\nНажмите Enter для выхода')
+        i_bar.next()
+        time.sleep(0.01)
+
+input(f'\n\nСписок уникальных комбинаций сохранен в файл "out.txt" в папке "data".\nНажмите Enter для выхода')
