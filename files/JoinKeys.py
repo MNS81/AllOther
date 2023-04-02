1a3724bac9df165ce44af1677b27d1da530bd531
MNS81 <mns81@bk.ru>
2023-04-03 00:23:23 +0300

    Программа получает списки ключей и объединяет их в отсортированный список уникальных комбинаций

--- files/JoinKeys.py
+++ files/JoinKeys.py
@@ -1,10 +1,8 @@
-import time
-from progress.bar import IncrementalBar
 with open('data/1.txt', encoding='utf-8') as first, open('data/2.txt', encoding='utf-8') as second, open('data/out.txt', 'w', encoding='utf-8') as outfile:
     print('*** Join Keys ***')
     print()
-    print('Для использования необходимо положить файлы "1.txt" и "2.txt" в папку "data".')
     print('Программа получает списки ключей и объединяет их в отсортированный список уникальных комбинаций.')
+    print('Для использования необходимо положить файлы "1.txt" и "2.txt" в папку "data".')
     print()
     lst_1 = first.read().splitlines()
     lst_2 = second.read().splitlines()
@@ -12,10 +10,6 @@ with open('data/1.txt', encoding='utf-8') as first, open('data/2.txt', encoding=
     for word in lst_1:
         for key in lst_2:
             lst_out.append(f'{word} {key}')
-    bar = IncrementalBar('Работаем: ', max=len(lst_out))
     for row in sorted(lst_out):
-        bar.next()
-        time.sleep(0.01)
         outfile.write(row + '\n')
-    bar.finish()
-input(f'\n\nСписок уникальных комбинаций сохранен в файл "out.txt" в папке "data".\n\nНажмите Enter для выхода')
+input(f'Список уникальных комбинаций сохранен в файл "out.txt" в папке "data".\n\nНажмите Enter для выхода')
